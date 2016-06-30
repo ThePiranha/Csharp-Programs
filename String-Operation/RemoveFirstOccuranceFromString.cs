a84a7bdcd9030d76d717eea89a951c0c3c78607e
ThePiranha <sabah_365@hotmail.com>
2016-06-30 09:50:56 -0400

    commit various way to solve

--- String-Operation/RemoveFirstOccuranceFromString.cs
+++ String-Operation/RemoveFirstOccuranceFromString.cs
@@ -27,3 +27,98 @@ namespace FirstOccuranceStringRemove
         }
     }
 }
+
+//Example: 02
+using System;
+
+namespace FirstOccuranceStringRemove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string main = @"C:\AceptaService\Peru_Cert\var\ca4xml_spool\output\dump\0628\1715\.20101461786 - 01 - F001 - 00000021.XML.0.xml";
+            string result = main.Remove(main.IndexOf('.'), 1);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
+
+//Example: 03
+using System;
+using System.IO;
+
+namespace FirstOccuranceStringRemove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string main = @"C:\AceptaService\Peru_Cert\var\ca4xml_spool\output\dump\0628\1715\.20101461786 - 01 - F001 - 00000021.XML.0.xml";
+            string filename = Path.GetFileName(main).TrimStart(new char[] { '.' });
+            string result = Path.Combine(Path.GetDirectoryName(main), filename);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
+
+//Example: 04
+using System;
+using System.Text;
+
+namespace FirstOccuranceStringRemove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool foundFirst = false;
+            StringBuilder temp = new StringBuilder();
+            string main = @"C:\AceptaService\Peru_Cert\var\ca4xml_spool\output\dump\0628\1715\.20101461786 - 01 - F001 - 00000021.XML.0.xml";
+            
+            foreach (char Character in main)
+            {
+                if (!foundFirst && Character == '.')
+                    foundFirst = true;
+                else
+                    temp.Append(Character);
+            }
+
+            string result = temp.ToString();
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
+
+//Example: 05
+using System;
+
+namespace FirstOccuranceStringRemove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result;
+            string main = @"C:\AceptaService\Peru_Cert\var\ca4xml_spool\output\dump\0628\1715\.20101461786 - 01 - F001 - 00000021.XML.0.xml";
+
+            if (main[main.LastIndexOf('\\') + 1] == '.')
+            {
+                result = "Path has a dot";
+            }
+            else
+            {
+                result = "Path does not has a dot";
+            }
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
